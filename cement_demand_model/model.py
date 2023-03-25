83cb016e0bb583e25d765b537cc4749723df277a
Sidnei Almeida <117240480+sid-almeida@users.noreply.github.com>
2023-03-24 23:47:01 -0300

    Update model.py

--- cement_demand_model/model.py
+++ cement_demand_model/model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-data = pd.read_csv('demand.csv')
+data = pd.read_csv('Demand2121.csv')
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
