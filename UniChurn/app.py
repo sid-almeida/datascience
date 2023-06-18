fbabef12159d211d185233c641224497896c706a
Sidnei Almeida <117240480+sid-almeida@users.noreply.github.com>
2023-06-18 16:25:46 -0300

    Update app.py

--- UniChurn/app.py
+++ UniChurn/app.py
@@ -266,6 +266,9 @@ if choice == "Previsão de Conjunto de Dados":
         st.subheader("Selecione as colunas alvo")
         colunas_selecionadas = st.multiselect("Colunas", dfp.columns)
         st.info("Selecione as colunas para prever a evasão dos alunos")
+        if colunas_selecionadas is None:
+            colunas_selecionadas = dfp.columns
+            st.info("Todas as colunas serão utilizadas para previsão.")
         if st.button("Prever"):
             import pickle
             model = pickle.load(open("model.pkl", "rb"))
